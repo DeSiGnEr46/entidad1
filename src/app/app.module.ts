74c3da3f230733943eb228f93d4562d527763fa3
Oliver López Olmedo <oliver@MBP-de-Oliver.home>
2020-01-04 21:12:18 +0100

    Added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/log/login/login.component';
+import { SignupComponent } from './componentes/log/signup/signup.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { PanelAdministracionComponent } from './componentes/panel-administracion/panel-administracion.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { EditarUsuarioComponent } from './componentes/editar-usuario/editar-usuario.component';
+import { EliminarUsuarioComponent } from './componentes/eliminar-usuario/eliminar-usuario.component';
+
+import { APP_ROUTING } from './app.routes';
+import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
+import { FooterComponent } from './componentes/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    PanelAdministracionComponent,
+    PerfilComponent,
+    EditarUsuarioComponent,
+    EliminarUsuarioComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/panel-administracion/panel-administracion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-administracion',
+  templateUrl: './panel-administracion.component.html',
+  styleUrls: ['./panel-administracion.component.scss']
+})
+export class PanelAdministracionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
